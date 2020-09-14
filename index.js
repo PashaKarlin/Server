9889f8cec7da0def9ca0f8d968de40e9c16fa160
Pasha Karlin <pashakarlin1234@gmail.com>
2020-09-14 23:38:32 +0300

    headers

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ server.use(express.json({ extended: false }))
 server.use('/api', require('./api/films'));
 
 server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Version, Authorization, Content-Type");
+  res.headers("Access-Control-Allow-Origin", "*");
+  res.headers("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.headers("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Version, Authorization, Content-Type");
   next();
 });
 
