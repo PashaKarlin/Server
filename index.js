15a7edc7142a7cf5c9a48b2d137688d917845551
Pasha Karlin <pashakarlin1234@gmail.com>
2020-09-14 23:28:08 +0300

    Accept,Version,Authorization,Content-Type

--- index.js
+++ index.js
@@ -15,46 +15,9 @@ server.use('/api', require('./api/films'));
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Version, Authorization, Content-Type");
   next();
 });
 
 server.listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`))
-
-
-
-
-
-// server.get('/', (req, res) => {
-//   res.send('Hello API')
-// })
-
-// server.get('/films', (req, res) => {
-//   res.send(films)
-// })
-// server.get('/films/:id', (req, res) => {
-//   // console.log(req.params)
-//   let film = films.find((film) => {
-//     return film.id === req.params.id
-//   })
-//   res.send(film)
-// })
-
-// server.post('/films', (req, res) => {
-//   film = {
-//     id: Date(),
-//     Title: req.body.Title,
-//   }
-//   films.push(film)
-//   console.log(req.params)
-//   res.sendStatus('200')
-// })
-
-// server.delete('/films/:title', (req, res) => {
-//   films = films.filter((film) => {
-//     return film.id !== (req.params.id);
-//   })
-//   res.sendStatus(200)
-// })
-
 
