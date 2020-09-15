65f787acc197b582d64057ebe9e4da2d62d7a3dc
Pasha Karlin <pashakarlin1234@gmail.com>
2020-09-15 19:06:40 +0300

    cors

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const connectDB = require('./Database/Connection')
 
 connectDB()
 server.use(bodyParser.urlencoded({ extended: false }))
-server.use(bodyParser.json())
+
 server.use(express.json({ extended: false }))
-server.use('/api', require('./api/films'));
+
 
 server.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header('Access-Control-Allow-Credentials', true);
-  res.header("Access-Control-Allow-Methods", 'OPTIONS,GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept, Version, Authorization,X-XSRF-TOKEN, Content-Type");
+  res.header("Access-Control-Allow-Methods", 'OPTIONS,GET,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,X-HTTP-Method-Override, Content-Type, Accept, Version, Authorization, X-XSRF-TOKEN, Content-Type");
   next();
 });
-
+server.use(bodyParser.json())
+server.use('/api', require('./api/films'));
 server.listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`))
 
